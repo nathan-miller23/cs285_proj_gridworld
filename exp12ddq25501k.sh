bcedd34f92f3b15026d2458e09467b2699f0bab8
abhi12-ayalur <abhinav.ayalur@gmail.com>
2020-12-13 13:25:38 -0800

    ddqn runs

--- exp12ddq25501k.sh
+++ exp12ddq25501k.sh
@@ -1,20 +1,20 @@
-python generate_data.py --save_agent --save_environment --epsilon 0.4 --delta 0.7 -o out
+python generate_data.py --save_agent --save_environment --epsilon 0.4 --delta 0.3 -o out
 for DATASET_SIZE in 250 500 1000
 do 
 	#python train.py -deep_q -i out -c -e 300 -n $DATASET_SIZE -s 1 -exp "Tabular{$DATASET_SIZE}"
-	python train.py -deep_q -i out -c -e 300 -n $DATASET_SIZE -s 1 -adv -exp "TabularAdv{$DATASET_SIZE}s1"
+	python train.py -empi -deep_q -i out -c -e 300 -n $DATASET_SIZE -s 1 -adv -exp "TabularAdv{$DATASET_SIZE}s1"
 done
 
-python generate_data.py --save_agent --save_environment --epsilon 0.4 --delta 0.7 -o out
+python generate_data.py --save_agent --save_environment --epsilon 0.4 --delta 0.3 -o out
 for DATASET_SIZE in 250 500 1000
 do 
 	#python train.py -deep_q -i out -c -e 300 -n $DATASET_SIZE -s 2 -exp "Tabular{$DATASET_SIZE}"
-	python train.py -deep_q -i out -c -e 300 -n $DATASET_SIZE -s 2 -adv -exp "TabularAdv{$DATASET_SIZE}s2"
+	python train.py -empi -deep_q -i out -c -e 300 -n $DATASET_SIZE -s 2 -adv -exp "TabularAdv{$DATASET_SIZE}s2"
 done
 
-python generate_data.py --save_agent --save_environment --epsilon 0.4 --delta 0.7 -o out
+python generate_data.py --save_agent --save_environment --epsilon 0.4 --delta 0.3 -o out
 for DATASET_SIZE in 250 500 1000
 do 
 	#python train.py -deep_q -i out -c -e 300 -n $DATASET_SIZE -s 3 -exp "Tabular{$DATASET_SIZE}"
-	python train.py -deep_q -i out -c -e 300 -n $DATASET_SIZE -s 3 -adv -exp "TabularAdv{$DATASET_SIZE}s3"
+	python train.py -empi -deep_q -i out -c -e 300 -n $DATASET_SIZE -s 3 -adv -exp "TabularAdv{$DATASET_SIZE}s3"
 done
