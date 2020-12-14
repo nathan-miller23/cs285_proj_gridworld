9b7f626ae7eaad1c9c625ae5cb57d9684ecc1239
Nathan Miller <nathan_miller23@berkeley.edu>
2020-12-14 15:07:05 -0500

    updated plot scipt

--- plot_constant_line_run.py
+++ plot_constant_line_run.py
@@ -22,31 +22,26 @@ from generate_data import DATA_DIR
 from agents import AgentFromTorch
 from train import *
 
-seeds = [1, 2, 3]
-dataset_sizes = [100, 250, 500, 1000, 2000, 4000]
-cmd_string = "python generate_data.py --save_agent --save_environment --epsilon 0.4 --delta 0.3"
-for seed in seeds:
-    for size in dataset_sizes:
-        logdir = os.path.join(LOG_DIR, "{}_{}".format("const"+ "{" + str(size) + "}" + "s" + str(seed), datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")))
-        print("Seed:", seed, "Size:", size)
-        print("generating data")
-        os.system(cmd_string)
-        print("data generated")
-        set_seed(seed)
-        exp_data_dir = os.path.join(DATA_DIR, "out")
-        data_load_loc = os.path.join(exp_data_dir, 'data.pkl')
-        writer = SummaryWriter(log_dir=logdir)
-        minepoch = 0
-        maxepoch = 300
-        #Loading data, env, agent
-        print("Loading data, env, agent")
-        X, Y, probs = load_data(data_load_loc, size, False, False, True)
-        print(probs)
-        print("Loaded data")
-        mean = 10*(probs[1] - probs[0]) / (probs[1]+probs[0])
-        writer.add_scalar("Reward/val_reward_mean", mean, minepoch)
-        writer.add_scalar("Reward/val_reward_mean", mean, maxepoch)
-        print("finished logging")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--probability_zero', '-p0', type=float, default=0.5)
+    parser.add_argument('--probability_one', '-p1', type=float, default=0.5)
+    parser.add_argument('--experiment_name', '-o', type=str, required=True)
+    args = parser.parse_args()
+    logdir = os.path.join(LOG_DIR, args.experiment_name)
+    writer = SummaryWriter(log_dir=logdir)
+    minepoch = 0
+    maxepoch = 300
+    #Loading data, env, agent
+    print("starting logging")
+    p0, p1 = args.probability_zero, args.probability_one
+    mean = 10*(p1 - p0) / (p1+p0)
+    print(mean)
+    for i in range(maxepoch):
+        writer.add_scalar("Reward/val_reward_mean", mean, i)
+    print("finished logging")
 
 
 
